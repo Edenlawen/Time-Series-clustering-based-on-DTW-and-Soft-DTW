0c0673637a3f9b90bdd5026e360b51aea4570013
Edenlawen <roudier.merlin@gmail.com>
2023-07-17 11:01:23 +0200

    main

---
+++ Projet_R/Python_function/EC_compareCourbe.py
@@ -0,0 +1,146 @@
+import numpy as np
+
+def compute_similariteAire(courbe1, courbe2):
+    simi = np.sum(1 / (1 + (courbe1 - courbe2) ** 2))
+    simi /= len(courbe1)
+    return simi
+
+def compute_Rcorrelation(courbe1, courbe2, verbose=False):
+    R = np.corrcoef(courbe2, courbe1)[0, 1]
+    if verbose:
+        if R ** 2 > 0.9 and R < 0.05:
+            print("acceptable model")
+        else:
+            print("not acceptable model R^2<0.9")
+    return R ** 2
+
+def compute_distMaxi(courbe1, courbe2):
+    dMax = np.max(np.abs(courbe2 - courbe1))
+    return dMax
+
+def compute_erreurFA2(courbe1, courbe2, verbose=False):
+    ratio = courbe2 / courbe1
+    fraction = ratio[(ratio >= 0.5) & (ratio <= 2)]
+    FA2 = len(fraction) / len(courbe1)
+    if verbose:
+        if FA2 > 0.8:
+            print("good model")
+        else:
+            print("important number of different point")
+    return FA2
+
+def compute_erreurFB(courbe1, courbe2, verbose=False):
+    m1 = np.mean(courbe1)
+    m2 = np.mean(courbe2)
+    FB = 2 * (m1 - m2) / (m1 + m2)
+    if verbose:
+        if np.abs(FB) < 0.3:
+            print("acceptable FB")
+        else:
+            print("non acceptable FB")
+    return FB
+
+def compute_erreurFS(courbe1, courbe2, verbose=False):
+    sd1 = np.std(courbe1)
+    sd2 = np.std(courbe2)
+    var1 = sd1 ** 2
+    var2 = sd2 ** 2
+    FS = 2 * (var1 - var2) / (var1 + var2)
+    if verbose:
+        if np.abs(FS) < 0.5:
+            print("acceptable model")
+        else:
+            print("non acceptable FS")
+    return FS
+
+def compute_erreurMG(courbe1, courbe2, verbose=False):
+    N = len(courbe1)
+    courbe1[courbe1 == 0] = 0.000001
+    courbe2[courbe2 == 0] = 0.000001
+    m1 = np.mean(np.log(courbe1))
+    m2 = np.mean(np.log(courbe2))
+    res = np.exp(m1 - m2)
+    if verbose:
+        if res <= 1.25 and res >= 0.75:
+            print("acceptable MG error")
+        else:
+            print("non acceptable MG error")
+    return res
+
+def compute_erreurNMSE(courbe1, courbe2, verbose=False):
+    N = len(courbe1)
+    Dif = np.mean((courbe1 - courbe2) ** 2)
+    m1 = np.mean(courbe1)
+    m2 = np.mean(courbe2)
+    res = Dif / (m1 * m2)
+    if verbose:
+        if res < 0.4:
+            print("acceptable NMSE error")
+        else:
+            print("non acceptable NMSE error")
+    return res
+
+def compute_erreurqn(courbe1, courbe2, verbose=False):
+    N = len(courbe1)
+    aire1 = np.sum((courbe1 - courbe2) ** 2)
+    aire2 = np.sum(courbe1 ** 2)
+    simi = np.sqrt(aire1 / aire2)
+    if verbose:
+        if simi < 0.4:
+            print("acceptable NMSE_O error")
+        else:
+            print("non acceptable NMSE_O error")
+    return simi
+
+def compute_erreurVG(courbe1, courbe2, verbose=False):
+    N = len(courbe1)
+    courbe1[courbe1 == 0] = 0.000001
+    courbe2[courbe2 == 0] = 0.000001
+    m1 = np.log(courbe1)
+    m2 = np.log(courbe2)
+    res = np.exp(np.mean((m1 - m2) ** 2))
+    if verbose:
+        if res <= 1.25 and res >= 0.75:
+            print("acceptable VG error")
+        else:
+            print("non acceptable VG error")
+    return res
+
+def compute_indicateurComp(courbe1, courbe2, par_R2=0.7, par_FA2=0.8, par_FB=0.3, par_FS=0.05, par_NMSE=0.4,
+                           par_simAire=0.95, par_MGmin=0.75, par_MGmax=1.25, v=False):
+    mini = np.minimum(np.min(courbe1), np.min(courbe2))
+    if mini < 0:
+        courbe1 = courbe1 + np.abs(mini)
+        courbe2 = courbe2 + np.abs(mini)
+    res = {}
+    res['R2'] = compute_Rcorrelation(courbe1, courbe2, verbose=v)
+    res['FA2'] = compute_erreurFA2(courbe1, courbe2, verbose=v)
+    res['FB'] = compute_erreurFB(courbe1, courbe2, verbose=v)
+    res['FS'] = compute_erreurFS(courbe1, courbe2, verbose=v)
+    res['NMSE'] = compute_erreurNMSE(courbe1, courbe2, verbose=v)
+    res['NMSE_O'] = compute_erreurqn(courbe1, courbe2, verbose=v)
+    res['MG'] = compute_erreurMG(courbe1, courbe2, verbose=v)
+    res['VG'] = compute_erreurVG(courbe1, courbe2, verbose=v)
+    res['simAire'] = compute_similariteAire(courbe1, courbe2)
+    res['distMax'] = compute_distMaxi(courbe1, courbe2)
+    test = True
+    test &= res['R2'] >= par_R2
+    test &= res['FA2'] >= par_FA2
+    test &= np.abs(res['FB']) < par_FB
+    test &= np.abs(res['FS']) < par_FS
+    test &= res['NMSE'] < par_NMSE
+    test &= res['simAire'] > par_simAire
+    test &= res['MG'] >= par_MGmin
+    test &= res['VG'] >= par_MGmin
+    test &= res['VG'] <= par_MGmax
+    totT = sum([res[x] >= y for x, y in zip(['R2', 'FA2', 'FB', 'FS', 'NMSE', 'simAire', 'MG', 'VG'],
+                                            [par_R2, par_FA2, par_FB, par_FS, par_NMSE, par_simAire, par_MGmin,
+                                             par_MGmin])])
+    totF = 9 - totT
+    if test:
+        res['all'] = 1
+    else:
+        res['all'] = -1
+    res['Nombre de conditions vraies'] = totT
+    res['Nombre de conditions fausses'] = totF
+    return res
---
+++ Projet_R/Python_function/data_loading.py
@@ -0,0 +1,75 @@
+"""Time-series Generative Adversarial Networks (TimeGAN) Codebase.
+
+Reference: Jinsung Yoon, Daniel Jarrett, Mihaela van der Schaar, 
+"Time-series Generative Adversarial Networks," 
+Neural Information Processing Systems (NeurIPS), 2019.
+
+Paper link: https://papers.nips.cc/paper/8789-time-series-generative-adversarial-networks
+
+Last updated Date: April 24th 2020
+Code author: Jinsung Yoon (jsyoon0823@gmail.com)
+
+-----------------------------
+
+data_loading.py
+
+(0) MinMaxScaler: Min Max normalizer
+(1) real_data_loading: Load and preprocess real data
+"""
+
+## Necessary Packages
+import numpy as np
+
+
+def MinMaxScaler(data):
+  """Min Max normalizer.
+  
+  Args:
+    - data: original data
+  
+  Returns:
+    - norm_data: normalized data
+  """
+  numerator = data - np.min(data, 0)
+  denominator = np.max(data, 0) - np.min(data, 0)
+  norm_data = numerator / (denominator + 1e-7)
+  return norm_data
+    
+
+def real_data_loading (data_name, seq_len):
+  """Load and preprocess real-world datasets.
+  
+  Args:
+    - data_name: stock or energy
+    - seq_len: sequence length
+    
+  Returns:
+    - data: preprocessed data.
+  """  
+  assert data_name in ['stock','energy', 'co2', 'co2V', 'co2R', 'google']
+  
+  if data_name == 'co2R':
+    ori_data = np.loadtxt('co2R.csv', delimiter = ",",skiprows = 1)
+
+        
+  # Flip the data to make chronological data
+
+  max = np.max(ori_data)
+  min = np.min(ori_data)
+  # Normalize the data
+  ori_data = MinMaxScaler(ori_data)
+    
+  # Preprocess the dataset
+  temp_data = []    
+  # Cut data by sequence length
+  for i in range(0, len(ori_data) - seq_len):
+    _x = ori_data[i:i + seq_len]
+    temp_data.append(_x)
+        
+  # Mix the datasets (to make it similar to i.i.d)
+  idx = np.random.permutation(len(temp_data))    
+  data = []
+  for i in range(len(temp_data)):
+    data.append(temp_data[idx[i]])
+    
+  return data, max, min
